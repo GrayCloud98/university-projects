3516d8931fbbde344f7b6319a3e36f818312da5e
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-06-18 16:30:49 +0200

    basic landing page

---
+++ project-three/frontend/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="GenerIO UI", layout="centered")
+st.title("🧠 Project GenerIO")
+st.write("Welcome to the interface for 3D model generation.")
